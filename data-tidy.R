8db87fb80a2d3da80fa47fb8f68c69e29b6443a6
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-09 17:00:44 +0800

    annotation and visualization

--- data-tidy.R
+++ data-tidy.R
@@ -28,12 +28,16 @@ cosmid.t <- read.delim('../comsid hg19.txt',
 library(GenomicRanges)
 library(stringr)
 library(tidyr)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+seqinfo <- Seqinfo(genome = 'hg19')
 off.spotter$V1 <- str_c('chr',off.spotter$V1)
 names(off.spotter)[5:6] <- c('query','hit')
 off.spotter.gr <- GRanges(seqnames = off.spotter$V1,
                           strand = off.spotter$V2,
                           ranges = IRanges(start = off.spotter$V3,
                                            end = off.spotter$V4),
+                          seqinfo = seqinfo,
                           dplyr::select(off.spotter, 
                                         query, 
                                         hit))
@@ -41,7 +45,8 @@ off.spotter.gr <- GRanges(seqnames = off.spotter$V1,
 cas.offfinder.gr <- GRanges(seqnames = cas.offfinder$Chromosome,
                             strand = cas.offfinder$Direction,
                             ranges = IRanges(start = cas.offfinder$Position,
-                                             width = 22),
+                                             width = 23),
+                            seqinfo = seqinfo,
                             dplyr::select(cas.offfinder,
                                           X.Bulge.type,
                                           crRNA,
@@ -50,12 +55,14 @@ cas.offfinder.gr <- GRanges(seqnames = cas.offfinder$Chromosome,
                                           Bulge.Size))
 
 cosmid <- separate(cosmid, `Chr Position`, sep = ':', into = c('chromosome', 'position'))
+cosmid$chromosome <- stringr::str_replace_all(cosmid$chromosome, 'Chr', 'chr')
 cosmid <- separate(cosmid, position, sep = '-', into = c('position.start', 'position.end'))
 names(cosmid) <- str_replace_all(names(cosmid), ' ', '.')
 cosmid.gr <- GRanges(seqnames = cosmid$chromosome,
                      strand = str_sub(cosmid$Strand,1,1),
                      ranges = IRanges(start = as.integer(cosmid$position.start),
                                       end = as.integer(cosmid$position.end)),
+                     seqinfo = seqinfo,
                      dplyr::select(cosmid,
                                    Score,
                                    Query.tag,
@@ -66,10 +73,52 @@ cosmid.gr <- GRanges(seqnames = cosmid$chromosome,
                                    Cut.site))
 
 
+# gene annotation ---------------------------------------------------------
 
+library(VariantAnnotation)
+cosmid.gr.ann <- locateVariants(cosmid.gr, txdb, AllVariants())
+cas.offfinder.gr.ann  <- locateVariants(cas.offfinder.gr, txdb, AllVariants())
+off.spotter.gr.ann <- locateVariants(off.spotter.gr, txdb, AllVariants())
+cas.offfinder.gr.ann.c <- cas.offfinder.gr.ann[cas.offfinder.gr.ann$LOCATION == 'coding']
 
+# gene location visualization ---------------------------------------------
 
+  
+  
+  
+anntrack  <- AnnotationTrack(start = cas.offfinder.gr.ann.c,
+                             width = wd,
+                             strand=st, 
+                             chromosome=chr, 
+                             genome="hg19", 
+                             name="Crispr")
 
+library(biomaRt)
+f <- as.integer(stt - 30000)
+t <- as.integer(stt + 30000)
+bm <- useMart(host="grch37.ensembl.org", 
+              biomart="ENSEMBL_MART_ENSEMBL", 
+              dataset="hsapiens_gene_ensembl")
+biomTrack <- BiomartGeneRegionTrack(genome="hg19", 
+                                    chromosome=chr, 
+                                    start=f, 
+                                    end=t,
+                                    name="ENSEMBL", 
+                                    biomart=bm)
+
+itrack <- IdeogramTrack(genome = 'hg19',chromosome = chr)
+
+gatrack <- GenomeAxisTrack(distFromAxis = 15,labelPos="below")
+
+jpeg(str_c(i,"output.jpg"), width = 5000, height =3090,res = 720)
+plotTracks(list(itrack,gatrack,biomTrack,anntrack),
+           transcriptAnnotation="symbol",
+           sizes = c(1,1,5,1),
+           from = f,
+           to = t,
+           main = str_c(i,'-',refseq,'  ->  ',altseq),
+           cex.main = 0.8)
+dev.off()
 
 
 
