b1bb54818a4b817be24b6879f39656cb2af7a66f
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-10 22:15:48 +0800

    get the gene name

--- graspGeneInfo .R
+++ graspGeneInfo .R
@@ -18,9 +18,14 @@ geneid <- read_csv('../geneid.txt', col_names = F)
 for (i in seq_len(length(geneid$X1))) {
   
   weburl <- str_c('https://www.ncbi.nlm.nih.gov/gene/?term=',geneid$X1[i])
-  NCBIgene <- read_html(weburl, encoding = 'UTF-8') %>% 
-    html_nodes('body div div form div div div div div div div div dl dd') %>% html_text()
-  print(NCBIgene[10]) 
+  NCBIgene <- read_html(weburl, encoding = 'UTF-8') 
+  genesummary <- html_nodes(NCBIgene,'body div div form div div div div div div div div dl dd') %>% 
+    html_text()
+  genename <- html_nodes(NCBIgene, 'body div div form h1 span') %>% 
+    html_text()
+  print(genename)
+  print(genesummary[10]) 
+  
 }
 
 
