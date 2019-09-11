71f922fe102d1460acb8053d1d75aea06e050c34
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-11 09:55:05 +0800

    add gene card script

--- graspGeneInfo .R
+++ graspGeneInfo .R
@@ -15,24 +15,33 @@ if (!require(stringr)) {
 library(readr)
 
 geneid <- read_csv('../geneid.txt', col_names = F)
+geneinfo <- vector(mode = 'character',length = length(geneid$X1))
+genenames <- vector(mode = 'character',length = length(geneid$X1))
 for (i in seq_len(length(geneid$X1))) {
   
   weburl <- str_c('https://www.ncbi.nlm.nih.gov/gene/?term=',geneid$X1[i])
   NCBIgene <- read_html(weburl, encoding = 'UTF-8') 
   genesummary <- html_nodes(NCBIgene,'body div div form div div div div div div div div dl dd') %>% 
     html_text()
+  geneinfo[i] <- genesummary[10]
   genename <- html_nodes(NCBIgene, 'body div div form h1 span') %>% 
     html_text()
+  genenames[i] <- genename
   print(i)
   print(genename)
   print(genesummary[10]) 
   
+  
 }
+off.target.genes<- data.frame(name = genenames, info = geneinfo, stringsAsFactors = F)
 
-
-
-
-
+oldpath <- getwd()
+setwd(dir = '../')
+write.csv(off.target.genes, file = 'offtargetgenes.txt',)
+setwd(dir = oldpath)
+i <- 3
+genecardurl <- str_c('https://www.genecards.org/cgi-bin/carddisp.pl?gene=', off.target.genes$name[i])
+genecard.gene <- read_html(genecardurl, encoding ='UTF-8')
 
 
 
