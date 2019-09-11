4ce3f59c2c77fd2d84a84b77b1600531e6566e83
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-11 16:18:07 +0800

    fix

--- graspGeneInfo .R
+++ graspGeneInfo .R
@@ -1,3 +1,6 @@
+
+# Loader package ----------------------------------------------------------
+
 if (!require(httr)) {
   install.packages('httr')
   library(httr)
@@ -39,9 +42,9 @@ for (i in seq_len(length(geneid$X1))) {
   
   
 }
+off.target.genes<- data.frame(name = genenames, info = geneinfo, stringsAsFactors = F)
 
 # uniprot -----------------------------------------------------------------
-
 
 gene.disseases <- vector(mode = 'character',length = length(geneid$X1))
 gene.functions <- vector(mode = 'character',length = length(geneid$X1))
@@ -59,21 +62,28 @@ for (i in seq_len(length(off.target.genes$name))) {
   }
   gene.dissease <- html_nodes(uniprot.gene,'.disseaseDescription') %>% 
     html_text()
-  if (nchar(gene.dissease)==0 | length(gene.dissease) == 0) {
+  print(gene.dissease)
+  if (length(gene.dissease) == 0) {
     gene.dissease <- NA
   }else{
-    gene.dissease <- gene.dissease
+    gene.dissease
   }
   gene.disseases[i] <- str_c(gene.dissease, sep = '', collapse = '')
   gene.function <- html_nodes(uniprot.gene,'div.annotation:nth-child(2)') %>% 
     html_text()
+  print(gene.function)
+  if (length(gene.function) == 0) {
+    gene.function <- NA
+  }else{
+    gene.function
+  }
   gene.functions[i] <- gene.function
   print(i)
   print(gene.dissease)
   print(gene.function)
   
 }
-off.target.genes<- data.frame(name = genenames, info = geneinfo, stringsAsFactors = F)
+
 
 oldpath <- getwd()
 setwd(dir = '../')
