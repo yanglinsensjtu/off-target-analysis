dc603ae7717a043ac2eeaf0f6d93a5f810e781da
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-12 13:15:26 +0800

    jointcall data

--- wgs.R
+++ wgs.R
@@ -14,3 +14,16 @@ wgs.more100.gr <- GRanges(seqnames = wgs.more100.fix$CHROM,
                                            width = 400),
                           strand = rep('*', 18))
 
+wgs.less100.jc <- readVcfAsVRanges('../jointcall-WGS/JC_SNP_INDEL_recal.PASS.vcf-anno.for_crispr.vcf')
+wgs.more100.jc <- read.vcfR('../jointcall-WGS/R19026469LD01-T0502-CAR_diploidSV.vcf.PASS.vcf-anno.rm_MOCK.vcf')
+
+wgs.less100.jc.gr <- GRanges(seqnames = wgs.less100.jc@seqnames,
+                             ranges = IRanges(start = start(wgs.less100.jc@ranges)-200,
+                                              width = 400),
+                             strand = wgs.less100.jc@strand)
+wgs.more100.jc.fix <- as.data.frame(wgs.more100.jc@fix)
+wgs.more100.jc.gr <- GRanges(seqnames = wgs.more100.jc.fix$CHROM,
+                             ranges = IRanges(start = as.integer(as.character(wgs.more100.jc.fix$POS)) - 200,
+                                              width = 400),
+                             strand = rep('*',nrow(wgs.more100.jc.fix)))
+wgs.jc <- c(wgs.more100.jc.gr, wgs.less100.jc.gr)
