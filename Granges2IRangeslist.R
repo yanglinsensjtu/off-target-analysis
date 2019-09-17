26ce711fdf9072de396fd43cf476ab9accdcd1e3
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-17 21:01:40 +0800

    improve and perfect function

--- Granges2IRangeslist.R
+++ Granges2IRangeslist.R
@@ -1,10 +1,17 @@
+library(GenomicRanges)
 
-gr.obj <- predict.off.target.coding.site.ann.screen
-irangeslist <- vector('list',length(gr.obj))
-for (i in seq_len(length(gr.obj))) {
-  chr <- deparse(substitute(as.character(gr.obj@seqnames[i])))
-  temp <- IRangesList(chr = IRanges(start(gr.obj@ranges[i]),end(gr.obj@ranges[i])))
-  temp
-  irangeslist[i]  <-  temp
+gr.obj <- GRanges(seqnames = c('chr1','chr4','chr3'),
+                  IRanges(start = c(100, 200, 300),
+                          width = 2000))
+
+Granges2IRangeslist <- function(gr.obj){
+  irangeslist <- IRangesList()
+  name <- vector('character',length(gr.obj))
+  for (i in seq_len(length(gr.obj))) {
+    name[i] <- as.character(gr.obj@seqnames[i])
+    irangeslist[i] <-  IRanges(start(gr.obj@ranges[i]),end(gr.obj@ranges[i]))
+  }
+  names(irangeslist) <- name
+  irangeslist
 }
-irangeslist
+
