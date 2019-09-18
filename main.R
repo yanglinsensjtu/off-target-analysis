b7337d3158096afcf3a7dd9a33c056afb2da98ce
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-18 12:10:26 +0800

    move the plots to another file

--- main.R
+++ main.R
@@ -3,33 +3,8 @@ source('plot.function.R')
 source('wgs.R')
 source('sgRNA alignment.R')
 source('changeIRanges.R')
+source('Granges2IRangeslist.R')
 
-coding.obj <- cosmid.gr.ann.c.check.back
-
-
-for (i in seq_along(coding.obj)) {
-  
-  plotGviz(coding.obj = coding.obj,
-           i=i,
-           bounds = 10000,
-           title = str_c(coding.obj$Score[i],
-                         '-',
-                         coding.obj$Query.type[i],
-                         '-',
-                         coding.obj$Mismatch[i]),
-           path = '../cosmid/')
-}
-
-coding.obj <- cas.offfinder.gr.ann.c.check.back
-
-
-for (i in seq_along(coding.obj)) {
-  plotGviz(coding.obj = coding.obj,
-           i=i,
-           bounds = 20000,
-           title = cas.offfinder.gr.ann.c.check.back$DNA[i],
-           path = '../casofffinder/')
-}
 
 wgs <- c(wgs.less100.gr,wgs.more100.gr)
 findOverlaps(cosmid.gr, wgs)
@@ -45,15 +20,7 @@ offtarget.gr.wgs <- unique(offtarget.gr[wgs.offtarget@to])
 sgRNA.alignment(grange = offtarget.gr.wgs,
                 sgRNA = targetgene)#offtarget predict sgRNA align with the target sgRNA
 
-wgs.offtarget.joint.gr <- unique(wgs.jc[wgs.offtarget@from])
 
-for (i in seq_along(wgs.offtarget.joint.gr)) {
-  plotGviz(coding.obj = wgs.offtarget.joint.gr,
-           i=i,
-           bounds = 20000,
-           title = '',
-           path = '../wgs-offtarget-common/')
-}
 sgRNA.alignment(grange = wgs.offtarget.joint.gr,
                 sgRNA = targetgene)
 b <- findOverlaps(wgs.old.jc, wgs.offtarget.joint.gr)
@@ -76,27 +43,7 @@ predict.off.target.coding.site <- union(cas.offfinder.gr.ann.c, cosmid.gr.ann.c)
 sgRNA.alignment(grange = predict.off.target.coding.site.ann,
                 sgRNA = targetgene)
 
-for (i in seq_along(predict.off.target.coding.site.ann)) {
-  plotGviz(coding.obj = predict.off.target.coding.site.ann,
-           i=i,
-           bounds = 20000,
-           title = '',
-           path = '../offtarget predict site common/')
-}
-for (i in seq_along(predict.off.target.coding.site)) {
-  plotGviz(coding.obj = predict.off.target.coding.site,
-           i=i,
-           bounds = 20000,
-           title = '',
-           path = '../offtarget predict site common/2/')
-}
-for (i in seq_along(predict.off.target.coding.site.ann.screen)) {
-  plotGviz(coding.obj = predict.off.target.coding.site.ann.screen,
-           i=i,
-           bounds = 20000,
-           title = '',
-           path = '../offtarget predict site common/3/')
-}
+
 
 sequences <- changeIRanges(granges.obj = predict.off.target.coding.site.ann.screen,
                            upstream = 300, 
@@ -109,3 +56,4 @@ write(sequences.DNA, 'predict off target genes sequences.txt')
 setwd(dir = oldpath)
 
 source('readBamplot.R')
+
