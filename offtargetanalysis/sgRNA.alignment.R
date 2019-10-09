59550c9481bcaa73ee9970c6594ad7cc8fef3fbf
lsyang <yanglinsensjtu@126.com>
2019-10-09 11:51:23 +0800

    Create sgRNA.alignment.R
    add alignment script

---
+++ offtargetanalysis/sgRNA.alignment.R
@@ -0,0 +1,15 @@
+library(BSgenome.Hsapiens.UCSC.hg19)
+BS.hg19 <- BSgenome.Hsapiens.UCSC.hg19
+# sgRNA aliganment --------------------------------------------------------
+sgRNA.alignment <- function(grange = Grange.obj,
+                            sgRNA = targetgene){
+  off.target.seq <- getSeq(BS.hg19, unique(grange))
+  for (i in seq_len(length(unique(grange)))) {
+    print(i)
+    print(pairwiseAlignment(sgRNA, 
+                            off.target.seq[i],
+                            gapOpening = 0,
+                            gapExtension = 1))
+  }
+}
+
