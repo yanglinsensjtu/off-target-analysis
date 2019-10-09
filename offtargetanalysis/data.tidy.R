aa390491f19c5141ba1892a7b04831611ccc529b
lsyang <yanglinsensjtu@126.com>
2019-10-09 10:27:55 +0800

    add new data tidy file

---
+++ offtargetanalysis/data.tidy.R
@@ -0,0 +1,73 @@
+# read the raw data into the R --------------------------------------------
+library(readr)
+library(tibble)
+library(readxl)
+library(dplyr)
+off.spotter<- read.delim('../Off-Spotter.hg19.txt', 
+                         header = F, 
+                         stringsAsFactors = F) %>% 
+  as_tibble()
+
+write.csv(off.spotter, 
+          file = '../Off-Spotter.csv')
+cas.offfinder <- read.delim('../Cas-OFFinder.hg19.txt',
+                            header = T,
+                            stringsAsFactors = F) %>% 
+  as_tibble() 
+
+cosmid <- read_xlsx('../cosmid_hg19_kf09aq66ka_output.xlsx')
+
+cosmid.t <- read.delim('../comsid hg19.txt',
+                       header = F, 
+                       stringsAsFactors = F) %>% 
+  as_tibble()
+
+
+# transform the data into granges obj -------------------------------------
+
+library(GenomicRanges)
+library(stringr)
+library(tidyr)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+seqinfo <- Seqinfo(genome = 'hg19')
+off.spotter$V1 <- str_c('chr',off.spotter$V1)
+names(off.spotter)[5:6] <- c('query','hit')
+off.spotter.gr <- GRanges(seqnames = off.spotter$V1,
+                          strand = off.spotter$V2,
+                          ranges = IRanges(start = off.spotter$V3,
+                                           end = off.spotter$V4),
+                          seqinfo = seqinfo,
+                          dplyr::select(off.spotter, 
+                                        query, 
+                                        hit))
+
+cas.offfinder.gr <- GRanges(seqnames = cas.offfinder$Chromosome,
+                            strand = cas.offfinder$Direction,
+                            ranges = IRanges(start = cas.offfinder$Position,
+                                             width = 23),
+                            seqinfo = seqinfo,
+                            dplyr::select(cas.offfinder,
+                                          X.Bulge.type,
+                                          crRNA,
+                                          DNA,
+                                          Mismatches,
+                                          Bulge.Size))
+
+cosmid <- separate(cosmid, `Chr Position`, sep = ':', into = c('chromosome', 'position'))
+cosmid$chromosome <- stringr::str_replace_all(cosmid$chromosome, 'Chr', 'chr')
+cosmid <- separate(cosmid, position, sep = '-', into = c('position.start', 'position.end'))
+names(cosmid) <- str_replace_all(names(cosmid), ' ', '.')
+cosmid.gr <- GRanges(seqnames = cosmid$chromosome,
+                     strand = str_sub(cosmid$Strand,1,1),
+                     ranges = IRanges(start = as.integer(cosmid$position.start),
+                                      end = as.integer(cosmid$position.end)),
+                     seqinfo = seqinfo,
+                     dplyr::select(cosmid,
+                                   Score,
+                                   Query.tag,
+                                   Search.result,
+                                   Query.type,
+                                   Mismatch,
+                                   Ends.with.RG,
+                                   Cut.site))
