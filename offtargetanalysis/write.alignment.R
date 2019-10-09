282b2b2c6644b9c8e560ac32e00301018ba31b21
lsyang <yanglinsensjtu@126.com>
2019-10-09 16:30:17 +0800

    write the alignment results to a text

--- offtargetanalysis/write.alignment.R
+++ offtargetanalysis/write.alignment.R
@@ -3,34 +3,39 @@ library(readr)
 library(BSgenome.Hsapiens.UCSC.hg19)
 BS.hg19 <- BSgenome.Hsapiens.UCSC.hg19
 write.alignment2txt <- function(granges.ann = granges.ann,
-                                sgRNA = sgRNA){
+                                sgRNA = sgRNA,
+                                path = path){
   off.target.seq <- getSeq(BS.hg19, granges.ann)
   write_lines('****The sgRNA alignment with the off target sites***', 
-              path = '../align sgRNA target.txt')
+              path = path)
   for (i in seq_len(length(granges.ann))) {
     cat(paste(i, ' '))
     paln <- pairwiseAlignment(sgRNA, 
                               off.target.seq[i],
                               gapOpening = 0,
                               gapExtension = 1)
+    
     write_lines('------------------------', 
-                path = '../align sgRNA target.txt',
+                path = path,
+                append = T)
+    write_lines(paste('--',i,'--'), 
+                path = path,
                 append = T)
     write_lines(names(sgRNA), 
-                path = '../align sgRNA target.txt',
+                path = path,
                 append = T)
     write_lines(paste(granges.ann$GENEID[i], 
                       as.character(granges(granges.ann)[i])), 
-                path = '../align sgRNA target.txt',
+                path = path,
                 append = T)
     write_lines('.....', 
-                path = '../align sgRNA target.txt',
+                path = path,
                 append = T)
     write_lines(alignedPattern(paln), 
-                path = '../align sgRNA target.txt',
+                path = path,
                 append = T)
     write_lines(alignedSubject(paln), 
-                path = '../align sgRNA target.txt',
+                path = path,
                 append = T)
     
   }
