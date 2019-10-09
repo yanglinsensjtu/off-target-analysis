820a32afb0f06824ddb59b0785fc3a00db3bd94f
lsyang <yanglinsensjtu@126.com>
2019-10-09 14:22:02 +0800

    modify

--- offtargetanalysis/main.R
+++ offtargetanalysis/main.R
@@ -2,5 +2,11 @@ source('offtargetanalysis/sgRNA.alignment.R')
 source('offtargetanalysis/data.tidy.R')
 source('offtargetanalysis/target.anotation.R')
 source('offtargetanalysis/sgRNA.alignment.R')
+source('offtargetanalysis/write.alignment.R')
 sgRNA <- readDNAStringSet('../sgRNA sequence.fasta')
 sgRNA.alignment(cco.ann,sgRNA)
+write.alignment2txt(cco.ann,sgRNA)
+
+
+
+
