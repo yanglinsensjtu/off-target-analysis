f7072672c0a78b077d836a01ab755d61139561aa
lsyang <yanglinsensjtu@126.com>
2019-10-09 12:26:09 +0800

    Update sgRNA.alignment.R
    Increasing Fault Tolerance

--- offtargetanalysis/sgRNA.alignment.R
+++ offtargetanalysis/sgRNA.alignment.R
@@ -4,12 +4,30 @@ BS.hg19 <- BSgenome.Hsapiens.UCSC.hg19
 sgRNA.alignment <- function(grange = Grange.obj,
                             sgRNA = targetgene){
   off.target.seq <- getSeq(BS.hg19, unique(grange))
-  for (i in seq_len(length(unique(grange)))) {
-    print(i)
-    print(pairwiseAlignment(sgRNA, 
-                            off.target.seq[i],
-                            gapOpening = 0,
+  if (is.null(grange$GENEID)) {
+    print('There is no geneid')
+    for (i in seq_len(length(unique(grange)))) {
+      print(i)
+      print(pairwiseAlignment(sgRNA, 
+                              off.target.seq[i],
+                              gapOpening = 0,
+                              gapExtension = 1))
+    }
+  }else{
+    for (i in seq_len(length(unique(grange)))) {
+      print(i)
+      
+        if (is.na(grange$GENEID[i])) {
+          print('The geneid is NA')
+        }else{
+          print(grange$GENEID[i])
+        }
+      
+      print(pairwiseAlignment(sgRNA, 
+                              off.target.seq[i],
+                              gapOpening = 0,
                             gapExtension = 1))
+    }
   }
 }
 
